2faeb91f67c36f56c82dbd5f86f690dcf748b008
Ysaakue <isaacgondim82@gmail.com>
2019-11-21 18:20:45 -0300

    first commit

---
+++ src/test/java/folhaPagamento/FolhaPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package folhaPagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhaPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
